baac5cdde3dbeb9bd95d34a99ed3b4c6349507d4
vaibhavdaga <potter22071995.vd@gmail.com>
2016-04-01 00:11:11 +0530

    Rabbit-Cipher

---
+++ rabbit.cpp
@@ -0,0 +1,58 @@
+#include "rabbit.h"
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <iterator>
+#include <utility>
+#include <functional>
+using namespace std;
+
+#define MP(x, y) make_pair(x, y)
+#define SET(p) memset(p, -1, sizeof(p))
+#define CLR(p) memset(p, 0, sizeof(p))
+#define MEM(p, v) memset(p, v, sizeof(p))
+#define CPY(d, s) memcpy(d, s, sizeof(s))
+#define SZ(c) (int)c.size()
+#define PB(x) push_back(x)
+#define ff first
+#define ss second
+#define ll long long
+#define ld long double
+#define mod 1000000007
+#define inf 1061109567LL
+#define pii pair< int, int >
+#define pll pair< ll, ll >
+#define psi pair< string, int >
+int main(int argc, char const *argv[])
+{
+	for(int i=0;i<8;i++)
+		key[i] = 0;
+	vector <unsigned int> plaintext;
+	for(int i=0;i<8;i++)
+		plaintext.push_back(0);
+	reset();
+	key_setup();
+	vector <unsigned int> cipher;
+	cipher = encrypt(plaintext);
+	for(int i=0;i<cipher.size();i++){
+		printf("%02X %02X %02X %02X ",(cipher[i]&0x000000FF),(cipher[i]&0x0000FF00)>>8,
+		(cipher[i]&0x00FF0000)>>16,(cipher[i]&0xFF000000)>>24);
+	}
+	cout<<endl;
+	return 0;
+}
---
+++ rabbit.h
@@ -0,0 +1,85 @@
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+unsigned int key[8];
+unsigned int iv[4];
+unsigned int X[8];
+unsigned int C[8],old_C[8];
+unsigned int A[8];
+unsigned int carry;
+unsigned int rotate_left(unsigned int v, unsigned int k) {
+	return (k%32 == 0) ? v : ((v << k) | (v >> (32 - k)));
+}
+void reset(){
+	for(int i=0;i<8;i++){
+		X[i] = C[i] = 0;
+	}
+	A[0] = A[3] = A[6] = 0x4D34D34D;
+	A[1] = A[4] = A[7] = 0xD34D34D3;
+	A[2] = A[5] = 0x34D34D34;
+}
+void counter_system(){
+	long long temp;
+	long long b=0;
+	for(int i=0;i<8;i++) old_C[i]=C[i];
+
+	temp = (C[0] & 0xFFFFFFFFll) + (A[0] & 0xFFFFFFFFll) + carry;
+    C[0] = (unsigned int) (t & 0xFFFFFFFFll);
+
+	for(int i=1;i<8;i++) {
+		temp = (C[i] & 0xFFFFFFFFll) + (A[i] & 0xFFFFFFFFll) + (old_C[i-1]>C[i-1]);
+		C[i] = (unsigned int) (t & 0xFFFFFFFFll);
+	}
+	carry = (old_C[7]>C[7]);
+}
+void next_state(){
+	counter_system();
+	unsigned int g[8];
+	long long temp;
+	for(int i=0;i<8;i++) {
+		temp = (X[i] + C[i]) & 0xFFFFFFFF;
+		temp = temp*temp;
+		g[i] = (unsigned int)( ((temp) ^ (temp >> 32))& 0xFFFFFFFF);
+	}
+	for(int i=0;i<8;i++){
+		if(i&1)
+			X[i] = g[i] + rotate_left(g[(i+7)%8],8) + g[(i+6)%8];
+		else
+			X[i] = g[i] + rotate_left(g[(i+7)%8],16) + rotate_left(g[(i+6)%8],16);
+	}
+}
+void key_setup(){
+	for(int i=0;i<8;i++){
+		if(i&1){
+			X[i] = (key[(i+5)%8]<<16) | key[(i+4)%8];
+			C[i] = (key[i]<<16) | (key[(i+1)%8]);
+		}
+		else{
+			X[i] = (key[(i+1)%8]<<16) | key[i];
+			C[i] = (key[(i+4)%8]<<16) | (key[(i+5)%8]);
+		}
+	}
+    c=0;
+	for(int i=0;i<4;i++){
+		next_state();
+	}
+	for (int i = 0; i < 8; i++)
+        C[(i + 4) & 0x7] ^= X[i];
+
+}
+vector <unsigned int> encrypt(vector <unsigned int> plain_text){
+	vector <unsigned int> cipher_text;
+	for(int i=0;i<plain_text.size();){
+		next_state();
+		for(int j=0;j<8 && i<plain_text.size();j+=2,i++){
+			cipher_text.push_back(plain_text[i] ^ X[j] ^ (X[(j+5)%8]>>16) ^ (X[(j+3)%8]<<16) );
+		}
+	}
+	return cipher_text;
+}
