ba9caa32403d432b436622f038b8a32a0e08b02e
vaibhavdaga <potter22071995.vd@gmail.com>
2016-04-12 16:33:16 +0530

    Binary Conversion

---
+++ binary_convert.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+inline void convert(unsigned int val){
+	string s = "00000000000000000000000000000000";
+	int i=31;
+	while(val){
+		if(val&1){
+			s[i]+=1;
+		}
+		val/=2;
+		i--;
+	}
+	puts(s);
+}
